c2cc2eeb23dd9f66a475339682f02964893b5a1b
clydexmarty09 <cmarty@ucsd.edu>
2026-01-16 19:13:42 -0800

    added CartContext object and declared Ca rtProvider function. This function will hold cart state and define cart logic

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -19,10 +19,29 @@ export type CartItem = {
 
 // interface/menu for the cart 
 type CartContextValue = {  // CartContextValue is an object type 
-    items : CartItem[]; 
-    addToCart : (product: Product)=> void; 
-    removeFromCart : (productID: string)=> void; 
-    clearCart : ()=> void; 
-    getQuantity : (productId: string)=> number; 
-    totalItems : number; 
+    items : CartItem[];  // items is a list of CartItem
+    addToCart : (product: Product)=> void;  // function that takes in paramater product of type Product. returns nothing 
+    removeFromCart : (productID: string)=> void;  // function that takes in paramater productID of type string. returns nothing 
+    clearCart : ()=> void; // clears cart - return nothing 
+    getQuantity : (productId: string)=> number; // function that takes in parameter productID of type string. Returns a number
+    totalItems : number; // data -- totalItems of type number 
+}
+
+/*
+create the global pipe or global communication channel - set to null be default 
+createConext<CartContextValue | null> : context will eventually hold a value fo type 
+CartContextValue, but for now it might be null. This is because we passed in null as a default type 
+CartContext is an object with .Provider component  
+Provider is a React component that supplies real cart data, null is a fallback value returned when no Provided exists
+upon writing this, we create CartContext.Provider and CartContext.Consumer 
+*/
+const CartContext = createContext<CartContextValue | null>(null); 
+
+/*
+create CartProvider function - a function that holds cart state and define cart logic 
+This will also render the real Provider 
+*/
+
+export function CartProvider({children}: {children: React.ReactNode}) {
+
 }
