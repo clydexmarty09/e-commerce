769c8bd369e13fa57fb027e87c12fa301a8089d6
clydexmarty09 <cmarty@ucsd.edu>
2026-01-19 01:19:09 -0800

    finished editing cart page and ProductItem.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,6 +3,25 @@ import { useCart } from "@/context/CartContext";
 
 export default function CartPage() {
 
+    const { items } = useCart(); 
+    
+    if(items.length==0) {
+        return(
 
+            <p> Cart Is Empty</p>
+        ); 
+    }
 
+    return(
+        <div>
+        {items.map((item) => (
+
+            <div key ={item.product.id}>
+                <p> name: {item.product.name} </p>
+                <p> quantity: {item.quantity} </p>
+           </div>
+        ))}
+       </div> 
+    ); 
+    
 }
--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -2,15 +2,25 @@
 import { Product, useCart } from "@/context/CartContext"; 
 
 
+
 export default function ProductItem({product}: {product: Product}) {
     
-    const { addToCart } = useCart(); 
+    const { addToCart, clearCart, removeFromCart } = useCart(); 
+    
 
     return (
         <div> 
             <h2> {product.name} </h2>
             <button onClick={() => addToCart(product)}> 
-                Add to Cart
+                Add to Cart  
+            </button>
+          
+            <button onClick ={()=> clearCart()}>
+                Remove All Items
+            </button>
+
+            <button onClick ={()=> removeFromCart(product.id)}> 
+                Remove one item
             </button>
         </div>
     )
