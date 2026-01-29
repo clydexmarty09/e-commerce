12fdd53c2837a5c92b17b9460de3c336cb7e6956
clydexmarty09 <cmarty@ucsd.edu>
2026-01-29 00:38:44 -0800

    Added documentation comments

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -2,20 +2,21 @@
 import { useCart } from "@/context/CartContext";
 
 export default function CheckoutPage() {
-
     
-
     const { items, total } =  useCart(); 
     if (items.length === 0 ) {
         return <h1> Cart is Empty</h1>
     }
 
+    // reduce = takes many values and combines them into one value 
+    // syntax: array.reduce((accumulator, currentItem)=> {=> ...}, startingValue)
     const totalPrice = items.reduce((sum, item) => {
 
         return sum + item.product.price * item.quantity; 
 
     }, 0);
 
+    // map array in JSX, and use stable key
     return (
         <main> 
             <h1> Checkout</h1>
