2a054f2b1bdd03c6cc2bf33dc1de2ff35bff9f03
clydexmarty09 <cmarty@ucsd.edu>
2026-02-02 15:05:37 -0800

    added Navbar.tsx for navigation

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+"use client"; 
+import Link from "next/link"; 
+import { useCart } from "@/context/CartContext"; 
+
+export default function Nav() {
+
+    const { total } = useCart(); 
+
+    return (
+
+        <nav> 
+            <Link href="/"> Home</Link> { "|" }
+            <Link href="/cart"> Cart ({total})</Link>
+        </nav>
+
+    );  
+}
