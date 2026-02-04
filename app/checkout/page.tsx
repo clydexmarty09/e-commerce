bd385cd635fbf9ce1985751c9b9e68dfaee1689d
clydexmarty09 <cmarty@ucsd.edu>
2026-02-04 11:24:06 -0800

    added checkout logic in app/checkout/page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,11 +1,40 @@
 "use client"; 
-import { useCart } from "@/context/CartContext";
+import { useCart, Order } from "@/context/CartContext";
+import { useState } from "react"; 
+import Link from "next/link"; 
 
 export default function CheckoutPage() {
     
-    const { items, total } =  useCart(); 
+    const { items, total, placeOrder } =  useCart(); 
+    const [order, setOrder] = useState<Order | null>(null); 
+    
+    if(order) {
+        return (
+            <main> 
+                <h1> Order confirmed </h1>
+                <p> Order ID: {order.id} </p>
+                <p> Total Price: {order.totalPrice} </p>
+
+                <h2> Items </h2>
+                <ul> 
+                    {items.map((item)=> (
+                        <li key={item.product.id}> 
+                        {item.product.name} - qty: {item.quantity} 
+                        </li>
+                    ))}
+                </ul>
+                <Link href="/"> Continue Shopping</Link>
+            </main>
+        ); 
+    }
+    
     if (items.length === 0 ) {
-        return <h1> Cart is Empty</h1>
+        return  (
+        <main>
+            <h1> Cart is Empty</h1>
+            <Link href="/"> Back to products </Link>
+        </main>
+        ); 
     }
 
     // reduce = takes many values and combines them into one value 
@@ -32,7 +61,12 @@ export default function CheckoutPage() {
 
             <p> Total items: { total } </p>
             <p> Total price: ${ totalPrice} </p>
+            <button
+            onClick={()=> {
+                const newOrder = placeOrder(); 
+                setOrder(newOrder); 
+            }}> Place Order </button>
             
         </main>
-    )
+    ); 
 }
