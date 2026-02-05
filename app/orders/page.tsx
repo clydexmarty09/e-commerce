b7be93307c833c0cab61fd2382dc605480046f15
clydexmarty09 <cmarty@ucsd.edu>
2026-02-05 12:11:25 -0800

    added new order page - displays order information after user has confirmed an order

---
+++ app/orders/page.tsx
@@ -0,0 +1,35 @@
+"use client"; 
+
+import { useCart } from "@/context/CartContext"; 
+import Link from "next/link"; 
+
+export function OrderPage() {
+
+    const { orders } = useCart(); 
+    if (orders.length === 0) {
+        return (
+            <main> 
+                <h1> No orders yet</h1>
+                <Link href="/"> Back to Shop</Link>
+            </main>
+        ); 
+    }
+    else {
+        return (
+
+            <main> 
+                <h1> Order Information: </h1>
+                <ul> 
+                    {orders.map((order)=> (
+                        <li key={order.id}> 
+                            <p> ID: {order.id} </p>
+                            <p> Total Price: {order.totalPrice}</p>
+                            <Link href={`/orders/${order.id}`}> View Order</Link>
+                        </li>
+                    ))}
+                </ul>
+                <Link href="/"> Back to Shop</Link>
+            </main>
+        ); 
+    }
+}
