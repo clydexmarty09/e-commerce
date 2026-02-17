9282251a5bffd26dd0ad3ac5319aa2a6a2202a13
clydexmarty09 <cmarty@ucsd.edu>
2026-02-17 12:00:11 -0800

    messing around with tailwind css styling

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -9,17 +9,17 @@ export default function ProductItem({product}: {product: Product}) {
     
 
     return (
-        <div> 
-            <h2> {product.name} </h2>
-            <button onClick={() => addToCart(product)}> 
-                Add to Cart  
+        <div className="flex gap-8 space-x-2 space-y-3"> 
+            <h2 className="font-style: italic"> {product.name} </h2>
+            <button className="flex gap-2" onClick={() => addToCart(product)}> 
+                Add to Cart 
             </button>
           
-            <button onClick ={()=> clearCart()}>
+            <button className="flex gap-2" onClick ={()=> clearCart()}>
                 Remove All Items
             </button>
 
-            <button onClick ={()=> removeFromCart(product.id)}> 
+            <button className="flex gap-2 rounded-md border px-2 py-1 hover:bg-amber-300 hover:cursor-pointer" onClick ={()=> removeFromCart(product.id)}> 
                 Remove one item
             </button>
         </div>
