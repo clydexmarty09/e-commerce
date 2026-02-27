164d25a0cbe457b832550e74dc824029819dc538
clydexmarty09 <cmarty@ucsd.edu>
2026-02-26 18:49:28 -0800

    styling tweaks

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -28,40 +28,40 @@ export default function CartPage() {
         const lineSubtotal = item.quantity * item.product.price;
 
         return (
-            <div className="flex items-center justify-between gap-4 border border-green-500/30 rounded-md p-3" key={item.product.id}>
-            <div className="min-w-0"> 
-                <p className="font-medium truncate"> Name: {item.product.name} </p>
-                <p className="text-sm opacity-80"> Unit Price: {formatMoney(item.product.price)} </p>
-                <p> Subtotal: ${lineSubtotal.toFixed(2)} </p>
-            </div>
-            
-            <div className="flex items-center gap-2"> 
-                <button
-                    className="btn btn-hover border border-green-500/30"
-                    disabled={item.quantity === 1}
-                    onClick={() => removeFromCart(item.product.id)}
-                >
-                    [-]
-                </button>
+            <div className="bg-black flex items-center justify-between gap-4 border border-green-500/20 rounded-md p-3" key={item.product.id}>
+                <div className="min-w-0"> 
+                    <p className="font-medium truncate text-green-100"> Name: {item.product.name} </p>
+                    <p className="text-sm opacity-80 text-green-200/70"> Unit Price: {formatMoney(item.product.price)} </p>
+                    <p className="font-medium mt-4 text-green-100/80"> Subtotal: <span className="text-green-200/70"> ${lineSubtotal.toFixed(2)} </span></p>
+                </div>
+                
+                <div className="flex items-center gap-2"> 
+                    <button
+                        className="btn text-green-300 btn-hover border border-green-500/30"
+                        disabled={item.quantity === 1}
+                        onClick={() => removeFromCart(item.product.id)}
+                    >
+                        [-]
+                    </button>
 
-                <span className="w-8 text-center"> {item.quantity} </span>
+                    <span className="w-8 text-center"> {item.quantity} </span>
 
-                <button className="btn btn-hover border border-green-500/30" onClick={() => addToCart(item.product)}>[+]</button>
-                <button className="btn btn-hover border border-green-500/30" onClick={() => removeItem(item.product.id)}>Remove</button>
-            </div>
+                    <button className="text-green-300 btn btn-hover border border-green-500/30" onClick={() => addToCart(item.product)}>[+]</button>
+                    <button className="btn btn-hover border text-green-300 border-green-500/30" onClick={() => removeItem(item.product.id)}>Remove</button>
+                </div>
             </div>
         );
         })}
 
-        <button className="btn-hover not-only:my-6 btn border border-green-500/30" onClick={() => clearCart()}>Clear Cart</button>
+        <button className="text-green-300 btn-hover not-only:my-6 btn border border-green-500/30" onClick={() => clearCart()}>Clear Cart</button>
         <div className="w-full py-6"> 
-            <p className="font-semibold text-left"> Total items: {total} </p>
+            <p className="text-green-100 font-semibold text-left"> Total items: <span className="text-green-100/70">{ total} </span> </p>
    
-            <p className="text-xs opacity-70 text-left"> Total price: ${totalPrice.toFixed(2)} </p>
+            <p className="text-xs opacity-70 text-left text-green-200/80"> Total price: <span className="text-green-100/80">${totalPrice.toFixed(2)} </span> </p>
         </div>
         
         <div className="flex flex-col gap-6"> 
-            <Link className="btn btn-hover border border-green-500/30" href="/checkout"> Proceed to checkout </Link>
+            <Link className="p-2 text-green-300 btn transition inline-block btn-hover hover:bg-green-500/10 hover:text-green-200 border border-green-500/30 rounded-md" href="/checkout"> Proceed to checkout </Link>
             {/*<Link className="btn border border-green-500/30" href="/"> Continue Shopping</Link>*/}
         </div>
     </div>
--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -12,21 +12,21 @@ export default function ProductItem({product}: {product: Product}) {
         <main className="border border-green-500/20 bg-black rounded-xl p-5"> 
             <div className="flex items-start justify-between gap-4"> 
                 <div className="min--0">
-                    <h2 className="text-lg font-semibold text-green-100 truncatefont-style:italic"> {product.name} </h2>
+                    <h2 className="text-lg font-semibold text-green-100 truncatefont-style:italic"> {product.name} </h2>  <span className="font-sx text-green-300"> ${product.price} </span>
                     <p className="mt-1 text-sm text-green-200/70"> Manage this item in cart</p>
                 </div> 
             </div>
                 
                 <div className="flex flex-wrap mt-4 gap-2"> 
-                    <button className="inline-flex items-center justify-center rounded-md border border-green-500/30 bg-black/40 px-3 py-2 text-sm text-green-100 hover:border-green-500/50 hover:bg-black/60 active:scale-95 transition" onClick={() => addToCart(product)}> 
+                    <button className="btn-hover inline-flex items-center justify-center rounded-md border border-green-500/30 bg-black/40 px-3 py-2 text-sm text-green-100 hover:border-green-500/50 hover:bg-black/60 active:scale-95 transition" onClick={() => addToCart(product)}> 
                         Add to Cart 
                     </button>
                 
-                    <button className="inline-flex items-center justify-center rounded-md border border-green-500/30 bg-black/40 px-3 py-2 text-sm text-green-100 hover:border-green-500/50 hover:bg-black/60 active:scale-95 transition" onClick ={()=> clearCart()}>
+                    <button className="btn-hover inline-flex items-center justify-center rounded-md border border-green-500/30 bg-black/40 px-3 py-2 text-sm text-green-100 hover:border-green-500/50 hover:bg-black/60 active:scale-95 transition" onClick ={()=> clearCart()}>
                         Remove All Items
                     </button>
 
-                    <button className="inline-flex items-center justify-center rounded-md border border-green-500/30 bg-black/40 px-3 py-2 text-sm text-green-100 hover:border-green-500/50 hover:bg-black/60 active:scale-95 transition" onClick ={()=> removeFromCart(product.id)}> 
+                    <button className="btn-hover inline-flex items-center justify-center rounded-md border border-green-500/30 bg-black/40 px-3 py-2 text-sm text-green-100 hover:border-green-500/50 hover:bg-black/60 active:scale-95 transition" onClick ={()=> removeFromCart(product.id)}> 
                         Remove one item
                     </button>
                 </div>
